5ece749ed04201613b6c862de8c9e5506916264d
João Milone <103215515+milysj@users.noreply.github.com>
2025-10-25 09:04:11 -0300

    testes
    testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,13 +23,6 @@ export default function LandingPage() {
             className="h-screen flex flex-col text-white relative overflow-hidden bg-cover bg-center"
             style={{backgroundImage: `url('/img/background-image.png')`}} // Imagem de fundo
         >
-            <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "tvolq13xii");
-</script>
             <CoinRain/> {/* Animação de moedas caindo */}
 
             {/* Barra lateral fixa com ícones e tooltips */}
@@ -94,5 +87,13 @@ export default function LandingPage() {
                 </section>
             </div>
         </main>
+
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tvolq13xii");
+</script>
     );
 }
--- src/app/pages/cadastro/page.tsx
+++ src/app/pages/cadastro/page.tsx
@@ -3,13 +3,7 @@ import Cadastrar from "@/app/components/Cadastro"; // Importa o componente de ca
 export default function Home() {
     return (
         <>
-        <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "tvolq13xii");
-        </script>
+        
             {/* ===========================
           Container principal da página
           =========================== */}
@@ -19,5 +13,13 @@ export default function Home() {
                 <Cadastrar/>
             </div>
         </>
+
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tvolq13xii");
+        </script>
     );
 }
